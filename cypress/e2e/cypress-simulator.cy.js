95ed787a6045abffd8076e803548e12daa140857
felipevitalinobarra <felipevitalino96@gmail.com>
2025-10-30 10:04:40 -0300

    test: implement initial test cases for Cypress Simulator

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -1,5 +1,73 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Cypress Simulator', () => {
+  it('success', () => {
+
+  })
+
+  it('error: invalid cypress command', () => {
+    
+  })
+
+  it('warning', () =>{
+
+  })
+
+  it('error: valid command without parentheses', () => {
+    
+  })
+
+  it('help', () => {
+
+  })
+
+  it('maximize/minimize', () => {
+
+  })
+
+  it('logout', () => {
+
+  })
+
+  it('show and hide logout button', () => {
+
+  })
+
+  it('running... state', () => {
+
+  })
+
+  it('accept cookies', () => {
+
+  })
+
+  it('decline cookies', () => {
+
+  })
+
+  it('captcha button states', () => {
+
+  })
+
+  it('captcha error', () => {
+
+  })
+
+  it('run button - enabled/disabled state', () => {
+
+  })
+
+  it('reset textarea on logout and login', () => {
+
+  })
+
+  it('disabled run button on logout and login', () => {
+    
+  })
+
+  it('reset output on logout and login', () => {
+    
+  })
+
+  it('no cookies banner on the login page', () => {
+    
   })
 })
