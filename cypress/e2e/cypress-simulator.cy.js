1cea12838fe4b25dfbe0ec8f63c0c00e1292baab
felipevitalinobarra <felipevitalino96@gmail.com>
2025-11-04 09:57:13 -0300

    test: add warning scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -19,7 +19,7 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
   })
 
-  it.only("it shows an error when entering and running an invalid Cypress command (e.g., cy.run())", () => {
+  it("it shows an error when entering and running an invalid Cypress command (e.g., cy.run())", () => {
     cy.get("textarea[placeholder='Write your Cypress code here...']")
       .type("cy.run()")
     cy.contains("button", "Run").click()
@@ -30,8 +30,15 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
   })
 
-  it("warning", () =>{
+  it("shows a warning when entering and running a not-implemented Cypress command (e.g., cy.contains('Login'))", () =>{
+    cy.get("textarea[placeholder='Write your Cypress code here...']")
+      .type("cy.contains('Login')")
+    cy.contains("button", "Run").click()
 
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Warning:")
+      .and("contain", "The `cy.contains` command has not been implemented yet.")
+      .and("be.visible")
   })
 
   it("error: valid command without parentheses", () => {
