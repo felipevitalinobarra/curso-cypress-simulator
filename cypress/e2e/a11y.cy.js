9eef2c2a9f99cf59524cf6acaa52106a31c78a7c
felipevitalinobarra <felipevitalino96@gmail.com>
2025-11-09 19:03:12 -0300

    refactor: add retries and run the same test a hundred times

--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -9,14 +9,16 @@ describe("Cypress Simulator - A11y Checks", () => {
         cy.injectAxe()
     })
 
-    it("successfully simulates a Cypress command (e.g., cy.log('Yay!'))", () => {
-        cy.run("cy.log('Yay!')")
+    Cypress._.times(100, () => {
+        it("successfully simulates a Cypress command (e.g., cy.log('Yay!'))", () => {
+            cy.run("cy.log('Yay!')")
 
-        cy.get("#outputArea", { timeout: 6000 })
-            .should("contain", "Success:")
-            .and("contain", "cy.log('Yay!') // Logged message 'Yay!'")
-            .and("be.visible")
-        cy.checkA11y(".success")
+            cy.get("#outputArea", { timeout: 6000 })
+                .should("contain", "Success:")
+                .and("contain", "cy.log('Yay!') // Logged message 'Yay!'")
+                .and("be.visible")
+            cy.checkA11y(".success")
+        })
     })
 
     it("shows an error when entering and running an invalid Cypress command (e.g., cy.run())", () => {
@@ -56,7 +58,7 @@ describe("Cypress Simulator - A11y Checks", () => {
 
     it("maximizes and minimizes a simulation result", () => {
         cy.run("cy.log('Yay!')")
-        
+
         cy.get(".expand-collapse").click()
 
         cy.get("#outputArea", { timeout: 6000 })
